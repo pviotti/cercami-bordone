1f9d3adae394467ef0336d23b336d13f2ebbb475
Paolo Viotti <skypixel@gmail.com>
2023-02-16 22:15:25 +0000

    feat: upload mp3 files

--- uploader.py
+++ uploader.py
@@ -1,4 +1,5 @@
 import os
+import glob
 
 from azure.storage.blob import BlobServiceClient
 from dotenv import load_dotenv
@@ -13,8 +14,10 @@ container_name = "tienimibordone"
 def main():
     while True:
         res = input("Select [l,d,u,q]: ")
+
         if res == 'q':
             break
+
         elif res == 'l':
             try:
                 list_response = blob_service_client.get_container_client(container_name).list_blobs()
@@ -25,10 +28,21 @@ def main():
                 print(f"Total: {count} blobs")
             except Exception as e:
                 print("Failed to get the blob list in the container. Error:" + str(e))
+
         elif res == 'u':
-            print("not implemented")
+            prefix = input("Input the prefix of files to upload: ")
+            files = glob.glob(f"./episodes-cut/{prefix}*.mp3")
+
+            # TODO: parallelize
+            for file in files:
+                blob_name = file.split("/")[-1]
+                blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
+                with open(file, "rb") as data:
+                    blob_client.upload_blob(data)
+                print(f"Uploaded: {file}")
+
         elif res == 'd':
-            res = input(f"Delete all blobs in {container_name}? [y,n]")
+            res = input(f"Delete all blobs in {container_name}? [y,n]: ")
             if res == 'y':
                 container_client = blob_service_client.get_container_client(container_name)
                 blob_list = container_client.list_blobs()
