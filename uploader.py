85fbfb622ef5d0909988333bccb358354531a73f
Paolo Viotti <skypixel@gmail.com>
2023-02-18 07:39:46 +0000

    feat: parallelize upload

--- uploader.py
+++ uploader.py
@@ -1,8 +1,11 @@
 import os
 import glob
+from threading import Lock
+from concurrent.futures import ThreadPoolExecutor
 
 from azure.storage.blob import BlobServiceClient
 from dotenv import load_dotenv
+
 
 load_dotenv()
 connect_str = os.getenv('AZURE_STORAGE_CONNECTION_STRING')
@@ -10,10 +13,23 @@ connect_str = os.getenv('AZURE_STORAGE_CONNECTION_STRING')
 blob_service_client = BlobServiceClient.from_connection_string(connect_str)
 container_name = "tienimibordone"
 
+print_lock = Lock()
+
+def p(string: str):
+    with print_lock:
+        print(string)
+
+def upload_file(file_name: str):
+    blob_name = file_name.split("/")[-1]
+    blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
+    with open(file_name, "rb") as data:
+        blob_client.upload_blob(data)
+    p(f"Uploaded: {file_name}")
+
 
 def main():
     while True:
-        res = input("Select [l,d,u,q]: ")
+        res = input("Select [l,d,u,q] > ")
 
         if res == 'q':
             break
@@ -33,13 +49,10 @@ def main():
             prefix = input("Input the prefix of files to upload: ")
             files = glob.glob(f"./episodes-cut/{prefix}*.mp3")
 
-            # TODO: parallelize
-            for file in files:
-                blob_name = file.split("/")[-1]
-                blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
-                with open(file, "rb") as data:
-                    blob_client.upload_blob(data)
-                print(f"Uploaded: {file}")
+            with ThreadPoolExecutor(10) as executor:
+                executor.map(upload_file, files)
+
+            print(f"Finished upload of {len(files)}.")
 
         elif res == 'd':
             res = input(f"Delete all blobs in {container_name}? [y,n]: ")
