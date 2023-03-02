f70720c83bcd07587dd5c4d482184a6c70d3e7bf
Paolo Viotti <skypixel@gmail.com>
2023-03-02 08:05:31 +0000

    feat: fe search result

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,34 +2,69 @@ import React from 'react';
 import logo from './images/cb_logo.jpg';
 import './App.css';
 
+type GrepResult = {
+    title: string;
+    url: string;
+    content: string;
+}
+
+function SearchResult({title, url, content}: GrepResult) {
+    return (
+        <div className='box'>
+            <div className='content is-small'>
+                <span className=''><a href={url}>{title}</a></span>
+                <blockquote className='blur-box'>
+                    ...{content}...
+                </blockquote>
+            </div>
+
+        </div>
+    );
+}
+
+
+
 function App() {
     return (
-        <section className="section is-medium">
-            <div className="columns is-centered">
-                <div className="column is-two-thirds">
+        <>
+            <section className="section is-medium">
+                <div className="columns is-centered">
+                    <div className="column is-two-thirds">
 
-                    <div className='section is-small columns is-centered image'>
-                        <figure className="image">
-                            <img src={logo} className="" alt="logo" />
-                        </figure>
-                    </div>
+                        <div className='section is-small columns is-centered image'>
+                            <figure className="image">
+                                <img src={logo} className="" alt="logo" />
+                            </figure>
+                        </div>
 
-                    <div className="columns is-centered">
-                        <div className="field has-addons">
-                            <div className="control">
-                                <input className="input" type="text" placeholder="" />
-                            </div>
-                            <div className="control">
-                                <a className="button is-info">
-                                    Cerca
-                                </a>
+                        <div className="columns is-centered">
+                            <div className="field has-addons">
+                                <div className="control">
+                                    <input className="input" type="text" placeholder="" />
+                                </div>
+                                <div className="control">
+                                    <a className="button is-info">
+                                        Cerca
+                                    </a>
+                                </div>
                             </div>
                         </div>
-                    </div>
 
+                    </div>
                 </div>
-            </div>
-        </section>
+            </section>
+            <section>
+                <div className="columns is-centered">
+                    <div className="column is-two-thirds">
+                        <SearchResult
+                            title="Title of the episode"
+                            url="https://duck.com"
+                            content="Ut venenatis, nisl scelerisque sollicitudin fermentum, quam libero hendrerit ipsum,
+                            ut blandit est tellus sit amet turpis"/>
+                    </div>
+                </div>
+            </section>
+        </>
     );
 }
 
