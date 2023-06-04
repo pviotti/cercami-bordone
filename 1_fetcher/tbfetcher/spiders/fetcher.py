b5542b46497c84cceaf2e1d213c2c75385307ad6
Paolo Viotti <skypixel@gmail.com>
2023-06-04 11:41:51 +0100

    fix: tbfetcher logic to use ajax request
    IlPost changed its authentication and the way they fetch podcast files
    from their servers. This commits updates the crawler accordingly.

--- 1_fetcher/tbfetcher/spiders/fetcher.py
+++ 1_fetcher/tbfetcher/spiders/fetcher.py
@@ -1,5 +1,10 @@
 import os
-import scrapy
+import calendar
+import json
+from datetime import datetime, date, timedelta
+from typing import Optional, cast
+
+from scrapy import Spider, FormRequest, Request
 from scrapy.http import TextResponse
 
 from ..items import TbfetcherItem
@@ -9,21 +14,43 @@ from dotenv import load_dotenv
 
 load_dotenv(dotenv_path="../.env", verbose=True)
 
-class TbPodcastSpider(scrapy.Spider):
+
+def get_last_day_of_month_before_previous(curr_day: date=date.today()) -> date:
+    first_day_of_current_month = date(curr_day.year, curr_day.month, 1)
+    last_day_of_previous_month = first_day_of_current_month - timedelta(days=1)
+    first_day_of_previous_month = date(last_day_of_previous_month.year,
+                                       last_day_of_previous_month.month, 1)
+    return first_day_of_previous_month - timedelta(days=1)
+
+
+class TbPodcastSpider(Spider):
     name = 'tbfetcher'
-    start_urls = ['https://www.ilpost.it/wp-login.php']
 
+    def __init__(self, year: Optional[str] = None, month: Optional[str] = None, *args, **kwargs):
+        super(TbPodcastSpider, self).__init__(*args, **kwargs)
+        if year is None or month is None:
+            self.limit_date = get_last_day_of_month_before_previous()
+        else:
+            year = int(year)
+            month = int(month)
+            self.limit_date = date(
+                year, month, calendar.monthrange(year, month)[1])
+        self.start_urls = ['https://www.ilpost.it/wp-login.php']
 
-    def parse(self, response: TextResponse):
-        return scrapy.FormRequest.from_response(
-            response,
-            formdata = {
-                'log': os.environ["ILPOST_USER"],
-                'pwd': os.environ["ILPOST_PASS"]
+    def parse(self, _: TextResponse):
+        username = os.environ["ILPOST_USER"]
+        password = os.environ["ILPOST_PASS"]
+        return [FormRequest(
+            url="https://www.ilpost.it/wp-login.php",
+            formdata={
+                'log': username,
+                'pwd': password,
+                'rememberme': 'forever',
+                'redirect_to': 'https://www.ilpost.it/podcasts/tienimi-bordone/',
+                'testcookie': "1",
             },
             callback=self.after_login
-        )
-
+        )]
 
     def after_login(self, response: TextResponse):
         login_error = response.css("div#login_error").extract_first()
@@ -31,25 +58,28 @@ class TbPodcastSpider(scrapy.Spider):
             print(f">>> Login error: {login_error}")
             return
 
-        print(">>> Login succedeed!")
-        return scrapy.Request(
-                url="https://www.ilpost.it/podcasts/tienimi-bordone/",
-                callback=self.parse_page)
+        request = cast(FormRequest, response.request)
+        request_cookies = cast(bytes, request.headers["Cookie"]).decode().split()
+        loggedin_cookie = [c for c in request_cookies if "wordpress_logged_in" in c][0]
+        loggedin_cookie_key = loggedin_cookie.split("=")[0]
+        rdata = f"action=checkpodcast&cookie={loggedin_cookie_key}&post_id=0&podcast_id=227193"
+        yield Request(url="https://www.ilpost.it/wp-admin/admin-ajax.php",
+                      method="POST",
+                      body=rdata,
+                      headers={
+                          "Accept": "application/json, text/javascript, */*; q=0.01",
+                          "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+                          "Content-Length": len(rdata)
+                      },
+                      callback=self.parse_json)
 
-
-    def parse_page(self, response: TextResponse):
-        play_elements = [el for el in response.css("a.play") if "data-file" in el.attrib]
-        for element in play_elements:
-            item = TbfetcherItem()
-            item["file_urls"] = [element.attrib["data-url"]]
-            item["desc"] = element.attrib["data-desc"]
-            item["title"] = element.attrib["data-title"]
-            yield item
-
-        next_page = response.css('a.next::attr(href)').extract_first()
-        print(f">>> Next page: {next_page}")
-        if next_page:
-            yield scrapy.Request(
-                url=f"https://www.ilpost.it{next_page}",
-                callback=self.parse_page
-            )
+    def parse_json(self, response: TextResponse):
+        data = json.loads(response.text)["data"]
+        for episode in data["postcastList"]:
+            episode_date = datetime.strptime(episode['date'], "%Y-%m-%d %H:%M:%S").date()
+            if episode_date > self.limit_date:
+                item = TbfetcherItem()
+                item["file_urls"] = [episode['podcast_raw_url']]
+                item["date"] = episode_date
+                item["title"] = episode["title"]
+                yield item
